c1f2b5d3a901de36bf73a5eea12c3df8b92446e4
Seema <seemaullal@gmail.com>
2015-02-05 11:34:05 -0500

    Recognoizes equilateral triangles

---
+++ triangle/triangle.js
@@ -0,0 +1,15 @@
+function Triangle(side1,side2,side3) {
+	this.side1 = side1;
+	this.side2 = side2;
+	this.side3 = side3;
+}
+
+Triangle.prototype.kind = function() {
+
+	if ( this.side1 === this.side2 && this.side2 === this.side3 ) {
+		return 'equilateral';
+	}
+
+}
+
+module.exports = Triangle;
