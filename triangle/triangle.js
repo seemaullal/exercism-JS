21e2ea59daffe1879fed57d3943fdb705c970f72
Seema <seemaullal@gmail.com>
2015-02-05 13:44:59 -0500

    Throws error if triangle inequality violated or if side length is 0

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -9,7 +9,10 @@ function Triangle(side1,side2,side3) {
 }
 
 Triangle.prototype.kind = function() {
-
+	if (this.sides[0] === 0)
+		throw "Invalid side length of 0"
+	if ( this.sides[0] + this.sides[1] <= this.sides[2])
+		throw "Violates triangle inequality"
 	if ( this.sides[0] === this.sides[2] )
 		return 'equilateral';
 	if ( this.sides[0] === this.sides[1] || 
