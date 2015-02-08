973b2844df6c143265c20b961218f79688af45ec
Seema <seemaullal@gmail.com>
2015-02-08 17:58:37 -0500

    Modified solution to pass all test cases and work for all roman numerals up to 3000 as test specified

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,6 +1,6 @@
 module.exports = function toRoman(num) {
 	var numToRoman = {
-	1 : 'I', 2 : "II", 3 : 'III', 4 : 'IV',
+	0: '', 1 : 'I', 2 : "II", 3 : 'III', 4 : 'IV',
 	5 : 'V', 6 : 'VI', 7 : 'VII', 8 : 'VIII',
 	9 : 'IX', 10 : 'X', 20 : 'XX', 30 : 'XXX',
 	40 : 'XL', 50 : 'L', 60 : 'LX', 70 : 'LXX',
