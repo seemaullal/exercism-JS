d5002633e862475ff8633e2796a41afe3aada292
Seema <seemaullal@gmail.com>
2015-02-08 17:57:59 -0500

    Recursive solution did not fit all test cases so implemented non-recursive solution which works for most (but not all) cases

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,30 +1,40 @@
 module.exports = function toRoman(num) {
-	if (num > 1000) 
-		return 'M' + toRoman(num-1000)
-	if (num === 1000) 
-		return 'M'
-	if (num > 500)
-		return 'D' + toRoman(num-500)
-	if (num === 500)
-		return 'D'
-	if (num > 100)
-		return 'C' + toRoman(num-100)
-	if (num === 100)
-		return 'C'
-	if (num > 50)
-		return 'L' + toRoman(num-50)
-	if (num === 50)
-		return 'L'
-	if (num > 10)
-		return 'X' + toRoman(num-10)
-	if (num === 10)
-		return 'X' 
-	if (num > 5)
-		return 'V' + toRoman(num-10);
-	if (num === 5)
-		return 'V'
-	if (num === 4)
-		return "IV"
-	if (num < 5)
-		return Array(num+1).join('I')
+	var numToRoman = {
+	1 : 'I', 2 : "II", 3 : 'III', 4 : 'IV',
+	5 : 'V', 6 : 'VI', 7 : 'VII', 8 : 'VIII',
+	9 : 'IX', 10 : 'X', 20 : 'XX', 30 : 'XXX',
+	40 : 'XL', 50 : 'L', 60 : 'LX', 70 : 'LXX',
+	80 : 'LXXX', 90 : 'XC', 100 : 'C', 200: 'CC',
+	300 : 'CCC', 400 : 'CD', 500 : 'D', 600: 'DC',
+	700 : 'DCC', 800 : 'DCCC', 900 : 'CM', 1000 : 'M',
+	2000 : 'MM', 3000: "MMM"
+	};
+
+	var digits = num.toString().split("");
+	var romanNumeral = "";
+	if (digits.length === 4) {
+		romanNumeral += numToRoman[digits[0]*1000];
+		romanNumeral += numToRoman[digits[1]*100];
+		romanNumeral += numToRoman[digits[2]*10];
+		romanNumeral += numToRoman[digits[3]*1];
+		return romanNumeral;
+	}
+
+	if (digits.length === 3) {
+		romanNumeral += numToRoman[digits[0]*100];
+		romanNumeral += numToRoman[digits[1]*10];
+		romanNumeral += numToRoman[digits[2]*1];
+		return romanNumeral;
+	}
+
+	if (digits.length === 2) {
+		romanNumeral += numToRoman[digits[0]*10];
+		romanNumeral += numToRoman[digits[1]*1];
+		return romanNumeral;
+	}
+
+	if (digits.length === 1) {
+		romanNumeral += numToRoman[digits[0]*1];
+		return romanNumeral;
+	}
 }
