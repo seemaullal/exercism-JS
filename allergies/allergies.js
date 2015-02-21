abaff87201eb0d58a7c3520279a13a98174ef574
Seema <seemaullal@gmail.com>
2015-02-21 11:10:11 -0500

    Helper function works but outputs results in wrong order

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -5,45 +5,44 @@ function Allergies (size) {
 Allergies.prototype.list = function() {
 	var allergiesLeft = this.size;
 	var allergies = [ ];
-	if (this.size === 0 )
-		return allergies;
 	while (allergiesLeft) {
 		var res = getAllergies(allergiesLeft);
 		allergies.push(res[0]);
 		allergiesLeft = res[1];
 	}
+	return allergies;
 };
 
 function getAllergies(num) {
 	if (num >= 128) {
-		return ['cats',num-128];
+		return [{value: 'cats', num : 128},num-128];
 	}
 	if (num >= 64) {
-		return ['pollen',num-64];
+		return [{value: 'pollen', num : 64} , num-64];
 	}
 
 	if (num >= 32) {
-		return ['chocolate',num-32];
+		return [{value: 'chocolate', num : 32},num-32];
 	}
 
 	if (num >= 16) {
-		return ['tomatoes',num-16];
+		return [{value: 'tomatoes', num : 16},num-16];
 	}
 
 	if (num >= 8) {
-		return ['strawberries',num-8];
+		return [{value: 'strawberries', num : 8},num-8];
 	}
 
 	if (num >= 4) {
-		return ['shellfish',num-4];
+		return [{value: 'shellfish', num : 4 },num-4];
 	}
 
 	if (num >= 2) {
-		return ['peanuts',num-2];
+		return [{value: 'peanuts', num : 2 },num-2];
 	}
 
 	if (num >= 1) {
-		return ['eggs',num-1];
+		return [{value: 'eggs', num : 1 },num-1];
 	}
 
 
