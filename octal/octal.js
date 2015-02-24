556ccff7540b0b195e8b4f05d46a198468861795
Seema <seemaullal@gmail.com>
2015-02-24 01:18:02 -0500

    Returns 0 for invalid input values

--- octal/octal.js
+++ octal/octal.js
@@ -3,6 +3,10 @@ function Octal(input) {
 }
 
 Octal.prototype.toDecimal = function() {
+	if (this.value * 1 != this.value) {
+		//not a valid input
+		return 0; 
+	}
 	var digits = this.value.split('').reverse();
 	var decimalValue = 0;
 	digits.forEach(function(value,index)  {
