8f2cbcce2228b5d714c02e8bdb555f3ae94dd442
Seema <seemaullal@gmail.com>
2015-02-25 22:58:35 -0500

    Translate works when string begins with a vowel

---
+++ pig-latin/pig-latin.js
@@ -0,0 +1,8 @@
+module.exports = {
+	translate : function(stringToTranslate) {
+		vowels = ['a','e','i','o','u'];
+		//check if the first letter of the string is a vowel
+		if (vowels.indexOf(stringToTranslate.charAt(0) != -1)
+			return stringToTranslate + 'ay'
+	} 
+}
