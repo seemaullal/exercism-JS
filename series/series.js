a8bc1757b52a753d61e1bdb382ad4a71b37b010a
Seema <seemaullal@gmail.com>
2015-02-26 01:14:18 -0500

    Slice method works

--- series/series.js
+++ series/series.js
@@ -8,4 +8,14 @@ Series.prototype.getDigits = function() {
 	return this.input.split('').map(Number);
 }
 
+Series.prototype.slices = function(number) {
+	var sliceArray = [ ];
+	var curIndex = 0;
+	while (curIndex <= this.digits.length - number) {
+		sliceArray.push(this.digits.slice(curIndex,curIndex+number));
+		curIndex++;
+	}
+	return sliceArray;
+}
+
 module.exports = Series;
--- series/series_test.spec.js
+++ series/series_test.spec.js
@@ -21,32 +21,32 @@ describe('Series', function () {
       .toEqual([9, 3, 6, 9, 2, 3, 4, 6, 8]);
   });
 
-  xit('can slice by 1', function () {
+  it('can slice by 1', function () {
     expect(new Series('01234').slices(1))
       .toEqual([[0], [1], [2], [3], [4]]);
   });
 
-  xit('can slice by 2', function () {
+  it('can slice by 2', function () {
     expect(new Series('98273463').slices(2))
       .toEqual([[9, 8], [8, 2], [2, 7], [7, 3], [3, 4], [4, 6], [6, 3]]);
   });
 
-  xit('can slice by 3', function () {
+  it('can slice by 3', function () {
     expect(new Series('01234').slices(3))
       .toEqual([[0, 1, 2], [1, 2, 3], [2, 3, 4]]);
   });
 
-  xit('can slice by 3 with duplicate digits', function () {
+  it('can slice by 3 with duplicate digits', function () {
     expect(new Series('31001').slices(3))
       .toEqual([[3, 1, 0], [1, 0, 0], [0, 0, 1]]);
   });
 
-  xit('can slice by 4', function () {
+  it('can slice by 4', function () {
     expect(new Series('91274').slices(4))
       .toEqual([[9, 1, 2, 7], [1, 2, 7, 4]]);
   });
 
-  xit('can slice by 5', function () {
+  it('can slice by 5', function () {
     expect(new Series('81228').slices(5))
       .toEqual([[8, 1, 2, 2, 8]]);
   });
