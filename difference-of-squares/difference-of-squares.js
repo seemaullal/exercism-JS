342391ac41027384005104467021b883b6cbaa55
Seema <seemaullal@gmail.com>
2015-02-26 01:22:52 -0500

    Calculates square of sums correctly

---
+++ difference-of-squares/difference-of-squares.js
@@ -0,0 +1,21 @@
+function Squares(number) {
+	this.size = number;
+	this.squareOfSums = this.calculateSquareofSums();
+	this.sumOfSquares = this.calculateSumOfSquares();
+	this.difference = this.squareOfSums - this.sumOfSquares;
+}
+
+Squares.prototype.calculateSquareofSums = function() {
+	var sum = 0;
+	for (var i = 1 ; i <= this.size ; i++) {
+		sum += i;
+	}
+	return sum*sum;
+};
+
+Squares.prototype.calculateSumOfSquares = function() {
+	
+};
+
+
+module.exports = Squares;
